17c7840cf9c91e14c2708768d3293b285e857b7f
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-11-18 21:12:25 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/STD-DEEPANSHU/AlexaMusic",
+    "https://github.com/Kassim752/AlexaMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
